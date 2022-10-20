165df6ae0afefcb9a83bf25410ac26f61d3c75af
Airat K <kamairat@icloud.com>
2022-10-20 14:20:28 +0300

    Account Controller was implemented

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from app.controllers import authentication_controller
 from app.controllers import family_controller
 from app.controllers import user_controller
-from app.controllers import accounts_controller
+from app.controllers import account_controller
 from app.controllers import categories_controller
 from app.controllers import transactions_controller
 from app.controllers import budgets_controller
@@ -40,7 +40,7 @@ api.add_middleware(
 api.include_router(authentication_controller, tags=[ "authentication" ])
 api.include_router(family_controller, tags=[ "family" ])
 api.include_router(user_controller, tags=[ "user" ])
-api.include_router(accounts_controller, tags=[ "accounts" ])
+api.include_router(account_controller, tags=[ "accounts" ])
 api.include_router(categories_controller, tags=[ "categories" ])
 api.include_router(transactions_controller, tags=[ "transactions" ])
 api.include_router(budgets_controller, tags=[ "budgets" ])
--- app/controllers/user.py
+++ app/controllers/user.py
@@ -23,3 +23,27 @@ class CurrentUser(BaseModel):
 @user_controller.get("/current", response_model=CurrentUser)
 async def get_current_user(current_user: User = Depends(identify_user)):
     return CurrentUser.from_orm(obj=current_user)
+
+
+@user_controller.get("/summary")
+async def get_summary(current_user: User = Depends(identify_user)):
+    # TODO: There should be summary for current period & all the time.
+    #       It should include `Balance`, `Income` & `Outcome` only.
+
+    pass
+
+@user_controller.get("/last-7-days")
+async def get_last_7_days_highlight(current_user: User = Depends(identify_user)):
+    pass
+
+@user_controller.get("/monthly-trend")
+async def get_monthly_trend(current_user: User = Depends(identify_user)):
+    pass
+
+@user_controller.get("/balances")
+async def get_balances(current_user: User = Depends(identify_user)):
+    pass
+
+@user_controller.get("/accounts")
+async def get_accounts(current_user: User = Depends(identify_user)):
+    return current_user.accounts
